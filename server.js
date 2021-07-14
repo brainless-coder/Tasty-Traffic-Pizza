a3b72928fa80268b185cdeb21a835f8c3f2c658d
Arvind Prime <singhamish27@gmail.com>
2021-07-14 10:01:28 +0530

    added cart and auth page

--- server.js
+++ server.js
@@ -10,14 +10,27 @@ const PORT = process.env.PORT || 3000;
 // Assets
 app.use(express.static('public'));
 
-app.get('/', (req, res) => {
-    res.render('home');
-});
-
 // Set Template Engine
 app.use(expressLayouts);
 app.set('views', path.join(__dirname, 'resources/views'));
 app.set('view engine', 'ejs');
+
+// Routes
+app.get('/', (req, res) => {
+    res.render('home');
+});
+
+app.get('/cart', (req, res) => {
+    res.render('customers/cart');
+});
+
+app.get('/login', (req, res) => {
+    res.render('auth/login');
+});
+
+app.get('/register', (req, res) => {
+    res.render('auth/register');
+});
 
 
 app.listen(PORT, () => {
